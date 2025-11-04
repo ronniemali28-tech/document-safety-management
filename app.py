4844b0238daa94930d77ea9ae0c7af707b5bb022
Gaurav Prakash Ahirrao <ronniemali28@gmail.com>
2025-11-04 12:12:46 +0530

    Fix port binding for Render deployment

--- app.py
+++ app.py
@@ -163,5 +163,8 @@ def uploaded_file(username, filename):
     return send_from_directory(os.path.join(app.config['UPLOAD_FOLDER'], username), filename)
 
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
